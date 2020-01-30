64134c6a38510198aac1664c57e803e4f6026fc9
manoelsouzadev <manoel.souza280@gmail.com>
2020-01-30 18:32:39 -0300

    Adicionado model, controller e router para oraçoes

--- src/app.js
+++ src/app.js
@@ -6,11 +6,13 @@ const app = express();
 const db = require('../config/db');
 const culto = require('./models/culto');
 const campanha = require('./models/campanha');
+const oracao = require('./models/oracao');
 
 //Carrega as rotas
 const indexRoute = require('./routes/index-route');
 const cultoRoute = require('./routes/culto-route');
 const campanhaRoute = require('./routes/campanha-route');
+const oracaoRoute = require('./routes/oracao-route');
 
 mongoose.connect(   
   db.mongoURI
@@ -29,5 +31,6 @@ app.use(cors(corsOptions));
 app.use('/', indexRoute);
 app.use('/cultos', cultoRoute);
 app.use('/campanhas', campanhaRoute);
+app.use('/oracoes', oracaoRoute);
 
 module.exports = app;

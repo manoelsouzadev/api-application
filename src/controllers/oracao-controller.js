e572ebb84c0f4cfe6e1e9c8a1c0983c3362a5a22
manoelsouzadev <manoel.souza280@gmail.com>
2020-01-30 18:43:38 -0300

    Alterado em oracao-controller o nome do model de oracoes para oracao

--- src/controllers/oracao-controller.js
+++ src/controllers/oracao-controller.js
@@ -4,8 +4,8 @@ const mongoose = require('mongoose');
 const Oracao = mongoose.model('oracao');
 
 exports.post = (req, res, next) => {
-  var oracoes = new Oracao(req.body);
-  oracoes 
+  var oracao = new Oracao(req.body);
+  oracao 
     .save()
     .then(() => {
       res.status(201).send({

18dad6ff82e09ecd561e3e2299cfbc23b775de11
manoelsouzadev <manoel.souza280@gmail.com>
2020-01-30 18:28:51 -0300

    Removido tudo relacionado a oracao

--- src/app.js
+++ src/app.js
@@ -3,16 +3,14 @@ const bodyParser = require("body-parser");
 const mongoose = require('mongoose');
 const cors = require("cors");
 const app = express();
-const db = require('../config/db.js');
+const db = require('../config/db');
 const culto = require('./models/culto');
 const campanha = require('./models/campanha');
-const oracao = require('./models/oracao');
 
 //Carrega as rotas
 const indexRoute = require('./routes/index-route');
 const cultoRoute = require('./routes/culto-route');
 const campanhaRoute = require('./routes/campanha-route');
-const oracaoRoute = require('./routes/oracao-route');
 
 mongoose.connect(   
   db.mongoURI
@@ -31,6 +29,5 @@ app.use(cors(corsOptions));
 app.use('/', indexRoute);
 app.use('/cultos', cultoRoute);
 app.use('/campanhas', campanhaRoute);
-app.use('/oracoes', oracaoRoute);
 
 module.exports = app;

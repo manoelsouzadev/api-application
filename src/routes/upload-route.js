66d9e6770cfd553f1bf1a62a54de7cc1696f91a1
manoelsouzadev <manoel.souza280@gmail.com>
2022-10-12 19:59:09 -0300

    Ajuste no upload controller

--- src/routes/upload-route.js
+++ src/routes/upload-route.js
@@ -7,7 +7,8 @@ const multipartMiddleware = multipart({ uploadDir: "./uploads"});
 
 const router = express.Router();
 
-router.post('/', multipartMiddleware, uploadController.post);
+router.post('/', multipartMiddleware, uploadController.upload);
+router.get('/autorizar', uploadController.autorizar);
 router.get('/oauth2', uploadController.getOAuth);
 
 module.exports = router;
